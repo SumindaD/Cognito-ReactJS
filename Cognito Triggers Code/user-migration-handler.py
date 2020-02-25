c17da7b6d911c277805cb40ce58e58af7dbe976e
Suminda Desilva <suminda.desilva@auxentaad.com>
2020-02-25 18:00:51 +0530

    Added user migration handler code

---
+++ Cognito Triggers Code/user-migration-handler.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+    
+    if(event['triggerSource'] == 'UserMigration_Authentication'):
+        url = 'https://play2.hanalyticssolutions.com/api/login'
+        myobj = {
+                  "emailAddress": event['userName'],    
+                  "password": event['request']['password']
+                }
+        
+        x = requests.post(url, data = myobj)
+        
+        if x.status_code == 200:
+            event['response']['userAttributes'] = {
+                "email": event['userName'],
+                "email_verified": "true"
+            };
+        
+            event['response']['finalUserStatus'] = "RESET_REQUIRED"
+            event['response']['messageAction'] = "SUPPRESS"
+        else:
+            raise Exception('Bad Creds')
+            
+    # event['response']['messageAction'] = "SUPPRESS"
+    return event
